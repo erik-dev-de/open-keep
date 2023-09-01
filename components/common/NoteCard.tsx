b15825adf9293448f10591938d1fff51e060dc11
HOME\andre <dev.erik@tutamail.com>
2023-09-01 21:51:43 +0300

    feat: added prettier

--- components/common/NoteCard.tsx
+++ components/common/NoteCard.tsx
@@ -1,71 +1,72 @@
 import React, { useContext, useState } from "react";
 import { Box, Typography, Fade } from "@mui/material";
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { ThemeContext } from "../layout/Theme";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/store";
 
 type NoteCardProps = {
-    note: {
-        title: string,
-        content: string,
-        note_id: number,
-        user_id: number
-    },
-    onCardCheck: (id: number) => void
+  note: {
+    title: string;
+    content: string;
+    note_id: number;
+    user_id: number;
+  };
+
+  onCardCheck: (id: number) => void;
 };
 
-const NoteCard: React.FC<NoteCardProps> = ({
-    note,
-    onCardCheck
-}) => {
+const NoteCard: React.FC<NoteCardProps> = ({ note, onCardCheck }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const checkedNotes = useSelector((state: RootState) => state.checkedNotes);
 
-    const [isHovered, setIsHovered] = useState(false);
-    const checkedNotes = useSelector((state: RootState) => state.checkedNotes);
+  const { theme } = useContext(ThemeContext);
 
-    const { theme } = useContext(ThemeContext);
-
-    return (
-        <Fade in>
-            <Box
-                className="rounded-lg border p-4 flex flex-col"
-                sx={{
-                    position: 'relative',
-                    transition: '0.3s ease-in-out',
-                    zIndex: 0,
-                    borderColor: checkedNotes.checked.includes(note.note_id) ? theme.palette.primary.main : ''
-                }}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-            >
-                <Fade in={isHovered || checkedNotes.checked.includes(note.note_id)} timeout={200}>
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            top: '-14px',
-                            left: '-10px',
-                            zIndex: 1,
-                            cursor: 'pointer',
-                            backgroundColor: theme.palette.background.default,
-                            borderRadius: '100%',
-                        }}
-                        onClick={() => {
-                            onCardCheck(note.note_id);
-                        }}
-                    >
-                        <CheckCircleIcon />
-                    </Box>
-                </Fade>
-                <Typography variant="h6" sx={{ wordBreak: 'break-all' }}>
-                    {note.title}
-                </Typography>
-                <Typography className="mt-2 text-sm" sx={{ wordBreak: 'break-all' }}>
-                    {note.content}
-                </Typography>
-            </Box>
+  return (
+    <Fade in>
+      <Box
+        className="rounded-lg border p-4 flex flex-col"
+        sx={{
+          position: "relative",
+          transition: "0.3s ease-in-out",
+          zIndex: 0,
+          borderColor: checkedNotes.checked.includes(note.note_id)
+            ? theme.palette.primary.main
+            : "",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Fade
+          in={isHovered || checkedNotes.checked.includes(note.note_id)}
+          timeout={200}
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "-14px",
+              left: "-10px",
+              zIndex: 1,
+              cursor: "pointer",
+              backgroundColor: theme.palette.background.default,
+              borderRadius: "100%",
+            }}
+            onClick={() => {
+              onCardCheck(note.note_id);
+            }}
+          >
+            <CheckCircleIcon />
+          </Box>
         </Fade>
-
-    )
-}
+        <Typography variant="h6" sx={{ wordBreak: "break-all" }}>
+          {note.title}
+        </Typography>
+        <Typography className="mt-2 text-sm" sx={{ wordBreak: "break-all" }}>
+          {note.content}
+        </Typography>
+      </Box>
+    </Fade>
+  );
+};
 
 export default NoteCard;
